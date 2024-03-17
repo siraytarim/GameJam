23edc841a3195dc667ff42638305b7691a90569b
Sıray Tarım <99121035+siraytarim@users.noreply.github.com>
2024-03-17 14:03:17 +0300

    Update Hikaye.cs

--- Scripts/Hikaye.cs
+++ Scripts/Hikaye.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public class Hikaye : MonoBehaviour
 {
-    public Text textComponent; // Unity arayüzündeki Text bileþenini referans almak için bir deðiþken
-    public string yazilacakMetin; // Ekrana yazýlacak metni buraya atayýn
-    public float harfGecikmeSuresi = 0.1f; // Her harf arasýndaki gecikme süresi
+    public Text textComponent; 
+    public string yazilacakMetin;
+    public float harfGecikmeSuresi = 0.1f; 
 
     private void Start()
     {
@@ -16,13 +16,12 @@ public class Hikaye : MonoBehaviour
 
     IEnumerator YaziYazdir()
     {
-        // Metindeki her harfi tek tek yazdýrmak için döngü
+     
         for (int i = 0; i < yazilacakMetin.Length; i++)
         {
-            // Ekrana bir harf ekleyin
+        
             textComponent.text += yazilacakMetin[i];
 
-            // Belirli bir süre bekleyin
             yield return new WaitForSeconds(harfGecikmeSuresi);
         }
     }
