b1a19a91694b8ef9ef6bd5d2bc89f12d0bb685bc
Sıray Tarım <99121035+siraytarim@users.noreply.github.com>
2024-03-17 14:00:59 +0300

    Update wayPoint.cs

--- Scripts/wayPoint.cs
+++ Scripts/wayPoint.cs
@@ -6,11 +6,11 @@ using UnityEngine.SceneManagement;
 public class wayPoint : MonoBehaviour
 {
 
-    public Transform[] waypoints; // Waypoint'lerin listesi
-    public float speed = 2.0f; // Nesnenin hýzý
+    public Transform[] waypoints;
+    public float speed = 2.0f; 
     public int currentWaypointIndex = 0;
 
-//    public GameObject sonküp;
+
     Animator anim;
 
     private void Start()
@@ -21,29 +21,25 @@ public class wayPoint : MonoBehaviour
     bool esit = false;
     void Update()
     {
-        // Nesnenin waypoint'e olan uzaklýðýný hesaplayýn
         float distanceToWaypoint = Vector3.Distance(transform.position, waypoints[currentWaypointIndex].position);
 
-        // Nesne waypoint'e yeterince yakýnsa
         if (distanceToWaypoint < 0.1f)
-        {
-            // Bir sonraki waypoint'e geçin
+{
             currentWaypointIndex++;
 
-            // Eðer son waypoint'e ulaþtýysanýz, baþa dönün
+          
             if (currentWaypointIndex >= waypoints.Length)
             {
                 Destroy(gameObject);
             }
         }
 
-        // Nesneyi waypoint'e doðru hareket ettirin
         transform.position = Vector3.MoveTowards(transform.position, waypoints[currentWaypointIndex].position, speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "karanlýk")
+        if (other.gameObject.tag == "karanlÃ½k")
         {
             anim.SetTrigger("Play");
         }
